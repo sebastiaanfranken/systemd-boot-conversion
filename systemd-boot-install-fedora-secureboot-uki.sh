4eab49301100f9811dd280aa209b807510a8a2ca
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2023-08-04 13:48:56 +0200

    Deleted UKI scripts since they're not working.

--- systemd-boot-install-fedora-secureboot-uki.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-echo "This script is currently known to not work. The UKI generation part isn't stable right now."
-echo "Please use the non-UKI version(s) of this script for now."
-
-exit 0
